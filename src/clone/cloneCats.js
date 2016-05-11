23729921a93a0c61f0957a2a54d2f8585cb7cd01
Guntars Simanskis <guntars@diamondgraphics.ie>
2016-05-11 15:57:23 +0100

    More category way

--- src/clone/cloneCats.js
+++ src/clone/cloneCats.js
@@ -2,57 +2,76 @@
  * Created by guntars on 11/05/2016.
  */
 define(()=> {
-    'use strict';
+        'use strict';
+        function None() {
+            return {
+                toString(){
+                    return 'none';
+                }
+            };
+        }
 
-    function isSimple(obj) {
-        return null == obj || "object" != typeof obj;
+        function isNone(obj) {
+            return obj.toString && obj.toString() === 'none';
+        }
+
+        function isSimple(obj) {
+            return typeof obj == 'boolean' || null == obj || 'object' != typeof obj;
+        }
+
+        function isDate(obj) {
+            return obj instanceof Date;
+        }
+
+        function isArray(obj) {
+            return obj instanceof Array;
+        }
+
+        function isObject(obj) {
+            return obj instanceof Object;
+        }
+
+
+        function applySimple(simple) {
+            return ()=> simple
+        }
+
+        function applyDate(date) {
+            return ()=> {
+                let copy = new Date();
+                copy.setTime(date.getTime());
+                return copy
+            }
+        };
+        function applyArray(arr) {
+            return (fn)=> arr.map(node=>fn(node));
+        }
+
+        function applyObj(obj) {
+            return (fn)=> {
+                let copy = {};
+                Object.keys(obj).forEach(attr=> {
+                    copy[attr] = fn(obj[attr]);
+                })
+                return copy;
+            }
+        };
+
+
+        function functor(type, action) {
+            return (item)=> type(item) ? action(item) : ()=>None();
+        }
+
+        function apply(...args) {
+            let iterate = (obj)=> args.map(arg=>arg(obj)((children)=>iterate(children))).filter(arg=>!isNone(arg))[0];
+            return iterate;
+        };
+
+        function clone(obj) {
+            return apply(functor(isSimple, applySimple), functor(isArray, applyArray), functor(isDate, applyDate), functor(isObject, applyObj))(obj);
+            throw new Error("Unable to copy obj! Its type isn't supported.");
+        }
+
+        return clone;
     }
-
-    function isDate(obj) {
-        return obj instanceof Date;
-    }
-
-    function isArray(obj) {
-        return obj instanceof Array;
-    }
-
-    function isObject(obj) {
-        return obj instanceof Object;
-    }
-    
-    
-    
-    function applySimple(simple) {
-        return simple;
-    }
-
-    function applyDate(date) {
-        let copy = new Date();
-        copy.setTime(date.getTime());
-        return copy;
-    }
-
-    function applyArray(arr) {
-        return arr.map(node=>clone(node));
-    }
-
-    function applyObj(obj) {
-        let copy = {};
-        Object.keys(obj).forEach(attr=> {
-            copy[attr] = clone(obj[attr]);
-        })
-        return copy;
-    }
-
-    function apply(...args) {
-       return (item)=>args.filter(arg=>arg[0](item)).map(arg=>arg[1](item))[0];
-    };
-
-
-    function clone(obj) {
-        return apply([isSimple, applySimple],[isArray, applyArray],[isDate, applyDate],[isObject, applyObj])(obj);
-        throw new Error("Unable to copy obj! Its type isn't supported.");
-    }
-
-    return clone;
-});
+);
