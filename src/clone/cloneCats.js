e13bb6dbcd2c5f547ba4af2aa7b61e2a2d33ec5d
Guntars Simanskis <guntars@diamondgraphics.ie>
2016-05-12 17:05:27 +0100

    Added more optimise

--- src/clone/cloneCats.js
+++ src/clone/cloneCats.js
@@ -3,7 +3,7 @@
  */
 define(()=> {
         'use strict';
-        class None extends Object {
+        class None {
             toString() {
                 return '[object None]';
             }
@@ -19,70 +19,40 @@ define(()=> {
             };
         }
 
-        function isNone(obj) {
-            return obj.toString && obj.toString() === '[object None]';
+        let isNone = (obj)=> obj.toString && obj.toString() === '[object None]';
+        let isSimple = (obj)=> typeof obj == 'boolean' || null == obj || 'object' != typeof obj;
+        let isDate = (obj)=> Object.prototype.toString.call(obj) === '[object Date]';
+        let isArray = (obj)=> Object.prototype.toString.call(obj) === '[object Array]';
+        let isObject = (obj)=> Object.prototype.toString.call(obj) === '[object Object]';
+
+        let applySimple = (simple)=> ()=> simple;
+
+        let applyDate = (date)=> ()=> {
+            let copy = new Date();
+            copy.setTime(date.getTime());
+            return copy
         };
 
-        function isSimple(obj) {
-            return typeof obj == 'boolean' || null == obj || 'object' != typeof obj;
-        };
+        let applyArray = (arr)=> (fn)=> arr.map(node=>fn(node));
 
-        function isDate(obj) {
-            return Object.prototype.toString.call(obj) === '[object Date]';
-        };
-
-        function isArray(obj) {
-            return Object.prototype.toString.call(obj) === '[object Array]';
-        };
-
-        function isObject(obj) {
-            return Object.prototype.toString.call(obj) === '[object Object]';
-        };
-
-
-        function applySimple(simple) {
-            return ()=> simple
-        };
-
-        function applyDate(date) {
-            return ()=> {
-                let copy = new Date();
-                copy.setTime(date.getTime());
-                return copy
-            }
-        };
-
-        function applyArray(arr) {
-            return (fn)=> arr.map(node=>fn(node));
-        };
-
-        function applyObj(obj) {
-            return (fn)=> {
-                let copy = {};
-                Object.keys(obj).forEach(attr=> {
-                    copy[attr] = fn(obj[attr]);
-                })
-                return copy;
-            }
+        let applyObj = (obj)=> (fn)=> {
+            let copy = {};
+            Object.keys(obj).forEach(attr=> {
+                copy[attr] = fn(obj[attr]);
+            })
+            return copy;
         };
 
 
-        function getOrElse(list, obj) {
+        let getOrElse = (list, obj) => {
             if (list) {
                 let result = list[0](obj);
                 return !isNone(result) ? result : getOrElse(list[1], obj)
             }
         };
 
-        function functor(type, action) {
-            return (item)=> {
-                return type(item) ? action(item) : new None();
-            }
-        }
-
-        function iterate(list, fn) {
-            return (obj)=> fn(list, obj)(children=>iterate(list, fn)(children));
-        }
+        let functor = (type, action) => item=> type(item) ? action(item) : new None();
+        let iterate = (list, fn)=> (obj)=> fn(list, obj)(children=>iterate(list, fn)(children));
 
         return obj=> iterate(new List(functor(isSimple, applySimple), functor(isArray, applyArray), functor(isDate, applyDate), functor(isObject, applyObj)), (list, obj)=>getOrElse(list, obj))(obj);
     }
