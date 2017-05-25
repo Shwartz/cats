0b249f96a35f510ea970f7978c2b23af80e8bdc6
Guntars Simanskis <guntars@diamondgraphics.ie>
2017-05-25 19:15:44 +0100

    Added extra changes for cleaner reading

--- pattern/src/server.js
+++ pattern/src/server.js
@@ -6,26 +6,19 @@ const app = express();
 
 app.use('/dist', express.static(join(__dirname, 'dist')));
 app.use('/service-worker.js', express.static(__dirname + '/service-worker.js'));
-app.use('/products.json', express.static(__dirname + '/products.json'));
 
 app.use((req, res, next) => {
     routes.trigger({
         path:   req.originalUrl,
         method: req.method
     })
-        .then(async cb => {
-            let resp = await cb().unsafeRun();
-            res.send(resp);
-        })
-        .catch(() => next())
+        .then(async cb => res.send(await cb().unsafeRun()))
+        .catch(() => next());
 });
-
-app.use('/dist', express.static('dist'))
 
 app.get('/hello', (req, res) => {
     res.send('hello');
 });
-
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!')
